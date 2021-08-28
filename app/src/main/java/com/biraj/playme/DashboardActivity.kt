637aa29d5ea4bf743980d80598a8697e63976b34
Biraj-timilsina <birajtimilsina123@gmail.com>
2021-08-28 18:35:02 +0545

    dashboard

--- app/src/main/java/com/biraj/playme/DashboardActivity.kt
+++ app/src/main/java/com/biraj/playme/DashboardActivity.kt
@@ -1,18 +1,22 @@
 package com.biraj.playme
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class DashboardActivity : AppCompatActivity() {
-//    private val permissions = arrayOf(
-//        android.Manifest.permission.CAMERA,
-//        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
-//        android.Manifest.permission.ACCESS_FINE_LOCATION
-//    )
+    private val permissions = arrayOf(
+        android.Manifest.permission.CAMERA,
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        android.Manifest.permission.ACCESS_FINE_LOCATION
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
@@ -20,42 +24,42 @@ class DashboardActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.flContainer)
 
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-//        val appBarConfiguration = AppBarConfiguration(
-//            setOf(
-//                R.id.navigation_home,
-//                R.id.navigation_dashboard,
-//                R.id.navigation_notifications
-//
-//            )
-//        )
-//        setupActionBarWithNavController(navController, appBarConfiguration)
+//         Passing each menu ID as a set of Ids because each
+//         menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_fav,
+                R.id.navigation_noti
+
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-//        if (!hasPermission()) {
-//            requestPermission()
-//        }
-//
+        if (!hasPermission()) {
+            requestPermission()
+        }
+
     }
-//    private fun requestPermission() {
-//        ActivityCompat.requestPermissions(
-//            this@DashboardActivity,
-//            permissions, 1234
-//        )
-//    }
-//    private fun hasPermission(): Boolean {
-//        var hasPermission = true
-//        for (permission in permissions) {
-//            if (ActivityCompat.checkSelfPermission(
-//                    this,
-//                    permission
-//                ) != PackageManager.PERMISSION_GRANTED
-//            ) {
-//                hasPermission = false
-//            }
-//        }
-//        return hasPermission
-//    }
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this@DashboardActivity,
+            permissions, 1234
+        )
+    }
+    private fun hasPermission(): Boolean {
+        var hasPermission = true
+        for (permission in permissions) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                hasPermission = false
+            }
+        }
+        return hasPermission
+    }
     
 }
