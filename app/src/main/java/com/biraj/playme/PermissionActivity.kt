286f1852c9305ffe0b4aad1ab301557f26b6bed5
Biraj-timilsina <birajtimilsina123@gmail.com>
2021-08-29 18:38:55 +0545

    permission managed

--- app/src/main/java/com/biraj/playme/PermissionActivity.kt
+++ app/src/main/java/com/biraj/playme/PermissionActivity.kt
@@ -1,5 +1,6 @@
 package com.biraj.playme
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
@@ -15,10 +16,10 @@ class PermissionActivity : AppCompatActivity() {
         setContentView(R.layout.activity_permission)
 
         // Check for permission
-//        if
-//                (!hasPermission()) {
-//            requestPermission()
-//        }
+        if
+                (!hasPermission()) {
+            requestPermission()
+        }
     }
     private fun requestPermission() {
         ActivityCompat.requestPermissions(
@@ -26,17 +27,17 @@ class PermissionActivity : AppCompatActivity() {
             permissions, 234
         )
     }
-//    private fun hasPermission(): Boolean {
-//        var hasPermission = true
-//        for (permission in permissions) {
-//            if (ActivityCompat.checkSelfPermission(
-//                    this,
-//                    permission
-//                ) != PackageManager.PERMISSION_GRANTED
-//            ) {
-//                hasPermission = false
-//            }
-//        }
-//        return hasPermission
-//    }
+    private fun hasPermission(): Boolean {
+        var hasPermission = true
+        for (permission in permissions) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                hasPermission = false
+            }
+        }
+        return hasPermission
+    }
 }
