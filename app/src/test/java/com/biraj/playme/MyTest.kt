0a41c91e0bfc19b7b0d403c3f5a3113edeb770ab
Biraj-timilsina <birajtimilsina123@gmail.com>
2021-09-01 19:03:38 +0545

    a test done

--- app/src/test/java/com/biraj/playme/MyTest.kt
+++ app/src/test/java/com/biraj/playme/MyTest.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Test
 
+//This is the test
 class MyTest {
     private lateinit var userRepository :UserRepository
     @Test
@@ -16,16 +17,16 @@ class MyTest {
         val actualResult = response.success
         Assert.assertEquals(expectedResult, actualResult)
     }
-    @Test
-    fun registerUser() = runBlocking {
-        val user =
-            User(FullName = "Biraj", Address = "Kapan",PhoneNo = "123456", Username = "biraj", Password = "biraj")
-        userRepository = UserRepository()
-        val response = userRepository.registerUser(user)
-        val expectedResult = true
-        val actualResult = response.success
-        Assert.assertEquals(expectedResult, actualResult)
-    }
+//    @Test
+//    fun registerUser() = runBlocking {
+//        val user =
+//            User(FullName = "Biraj", Address = "Kapan",PhoneNo = "123456", Username = "biraj", Password = "biraj")
+//        userRepository = UserRepository()
+//        val response = userRepository.registerUser(user)
+//        val expectedResult = true
+//        val actualResult = response.success
+//        Assert.assertEquals(expectedResult, actualResult)
+//    }
 
 
 }
