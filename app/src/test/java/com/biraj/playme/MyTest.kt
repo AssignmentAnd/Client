33593df2021234f527acd0fe84404f896e4b6e1c
Biraj-timilsina <birajtimilsina123@gmail.com>
2021-09-02 19:04:22 +0545

    signup and login test

--- app/src/test/java/com/biraj/playme/MyTest.kt
+++ app/src/test/java/com/biraj/playme/MyTest.kt
@@ -17,16 +17,16 @@ class MyTest {
         val actualResult = response.success
         Assert.assertEquals(expectedResult, actualResult)
     }
-//    @Test
-//    fun registerUser() = runBlocking {
-//        val user =
-//            User(FullName = "Biraj", Address = "Kapan",PhoneNo = "123456", Username = "biraj", Password = "biraj")
-//        userRepository = UserRepository()
-//        val response = userRepository.registerUser(user)
-//        val expectedResult = true
-//        val actualResult = response.success
-//        Assert.assertEquals(expectedResult, actualResult)
-//    }
+    @Test
+    fun registerUser() = runBlocking {
+        val user =
+            User(FullName = "Biraj", Address = "Kapan",PhoneNo = "123456", Username = "biraj", Password = "biraj")
+        userRepository = UserRepository()
+        val response = userRepository.registerUser(user)
+        val expectedResult = true
+        val actualResult = response.success
+        Assert.assertEquals(expectedResult, actualResult)
+    }
 
 
 }
